ec5c7b71c159bafd14cf2258969cc4b6946c4424
Anastasiia <101048488+AAleksandrovna@users.noreply.github.com>
2024-06-29 09:14:06 +0200

    Finished app

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,41 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
+final class ViewController: UIViewController {
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
+    
     @IBOutlet var startNextButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.4
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startNextButton.layer.cornerRadius = 10
-        redLight.layer.cornerRadius = 70
-        yellowLight.layer.cornerRadius = 70
-        greenLight.layer.cornerRadius = 70
+                
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
 
     }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
 
-    @IBAction func switchingTrafficLights(_ sender: UIButton) {
-        sender.setTitle(
-            startNextButton.isTouchInside ? "NEXT" : "START",
-            for: .normal)
+    @IBAction func switchingTrafficLights() {
+        if startNextButton.currentTitle == "START" {
+            startNextButton.setTitle("NEXT", for: .normal)
+        }
         
-        switch startNextButton.isTouchInside {
-        case redLight.alpha == 1:
-            redLight.alpha = 0.4
-            yellowLight.alpha = 1
-        case yellowLight.alpha == 1:
-            yellowLight.alpha = 0.4
-            greenLight.alpha = 1
-        case greenLight.alpha == 1:
-            greenLight.alpha = 0.4
-            redLight.alpha = 1
-        default:
-            redLight.alpha = 1
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightIsOn
+            greenLight.alpha = lightIsOff
+            currentLight = .yellow
+        case .yellow:
+            yellowLight.alpha = lightIsOn
+            redLight.alpha = lightIsOff
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
         }
     }
     
 }
 
+// Лучший вариант
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
